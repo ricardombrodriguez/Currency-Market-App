5824ffa69ffd3219d74356d57a8d030e905531bd
Pedro Duarte <pedro.dld536@gmail.com>
2021-12-13 13:54:07 +0000

    First Commit

---
+++ projDataGenerator/BittrexEventHandlers.py
@@ -0,0 +1,27 @@
+from base64 import b64decode
+from zlib import decompress, MAX_WBITS
+
+from MQInstance import MQInstance
+
+
+class BittrexEventHandlers:
+
+  @classmethod
+  async def on_tickers(cls, msg) -> None:
+    cls._publish_decoded('Tickers', msg)
+
+  @classmethod
+  def _publish_decoded(cls, channel: str, msg) -> None:
+    cls._publish(channel, cls._process_message(msg[0]))
+
+  @classmethod
+  def _publish(cls, channel: str, body: str) -> None:
+    MQInstance.get_instance().publish(channel, body)
+
+  @classmethod
+  def _process_message(cls, message: str) -> None:
+    try:
+      decompressed_msg = decompress(b64decode(message, validate=True), -MAX_WBITS)
+    except SyntaxError:
+      decompressed_msg = decompress(b64decode(message, validate=True))
+    return decompressed_msg.decode()
