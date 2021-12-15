5da5c7a469456a8a764d7e702a884fd6fe3b387c
joaoreis16 <joaoreis16@ua.pt>
2021-12-15 23:24:23 +0000

    erro da qual nao sei de onde vem :/

--- finance/src/main/java/yes/finance/controller/FinanceController.java
+++ finance/src/main/java/yes/finance/controller/FinanceController.java
@@ -31,6 +31,8 @@ public class FinanceController {
     private TransactionService transactionservice;
     @Autowired
     private PortfolioService portfolioservice;
+    @Autowired
+    private TickerService tickerservice;
 
     @GetMapping("/users")
     public Page<User> getAllUsers(Pageable pageable) {
@@ -116,101 +118,15 @@ public class FinanceController {
     public Transaction createTransaction(@RequestBody Transaction transaction) {
         return transactionservice.saveTransaction(transaction);
     }
+
+
+    @GetMapping("/tickers")
+    public Page<Ticker> getAllTickers(Pageable pageable) {
+        return tickerservice.getTickers(pageable);
+    }
+
+    @PostMapping("/tickers")
+    public Ticker createTicker(@RequestBody Ticker ticker) {
+        return tickerservice.saveTicker(ticker);
+    }
 }
-
-/*
-
-
-@RestController
-public class QuotesController {
-    @Autowired
-    private QuotesRepository quotesRepository;
-    @Autowired
-    private MovieRepository moviesRepository;
-
-    // @GetMapping("/quotes")
-    // @ResponseBody
-    // public List<Quotes> getAllQuotes(@RequestParam(defaultValue = "all") Long id) {
-    //     if (id.equals("all")){
-    //         return quotesRepository.findAll();
-    //     }else{
-    //         return quotesRepository.findById(id);
-    //     }
-        
-    // }
-
-    @GetMapping("/quotes")
-    public List<Quotes> getAllQuotes() {
-        return quotesRepository.findAll();
-    }
-
-
-    @GetMapping("/quotes/{id}")
-    public ResponseEntity<Quotes> getQuotesById(@PathVariable(value = "id") Long quotesId)
-        throws ResourceNotFoundException {
-        Quotes quotes = quotesRepository.findById(quotesId)
-          .orElseThrow(() -> new ResourceNotFoundException("Quotes not found for this id :: " + quotesId));
-        return ResponseEntity.ok().body(quotes);
-    }
-    
-    @PostMapping("/quotes")
-    public Quotes createQuotes(@Valid @RequestBody Quotes quote) {
-        return quotesRepository.save(quote);
-    }
-
-
-    @PutMapping("/quotes/{id}")
-    public ResponseEntity<Quotes> updateEmployee(@PathVariable(value = "id") Long employeeId,
-         @Valid @RequestBody Quotes employeeDetails) throws ResourceNotFoundException {
-        Quotes employee = quotesRepository.findById(employeeId)
-        .orElseThrow(() -> new ResourceNotFoundException("Quote not found for this id :: " + employeeId));
-
-        employee.setTexto(employeeDetails.getTexto());
-        employee.setYear(employeeDetails.getYear());
-        final Quotes updatedEmployee = quotesRepository.save(employee);
-        return ResponseEntity.ok(updatedEmployee);
-    }
-
-    @DeleteMapping("/quotes/{id}")
-    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
-         throws ResourceNotFoundException {
-        Quotes employee = quotesRepository.findById(employeeId)
-       .orElseThrow(() -> new ResourceNotFoundException("Quote not found for this id :: " + employeeId));
-
-        quotesRepository.delete(employee);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return response;
-    }
-
-
-    // @GetMapping("/movies")
-    // @ResponseBody
-    // public List<Movie> getAllMovies() {
-    //     return movieRepository.findAll();
-    // }
-
-
-    @GetMapping("/movies")
-    public List<Movie> findAllMovies() {
-        return moviesRepository.findAll();
-    }
-
-    @GetMapping("/movies/{id}")
-    public ResponseEntity<Movie> getMovieById(@PathVariable(value = "id") Long movieId)
-        throws ResourceNotFoundException {
-        Movie movie = moviesRepository.findById(movieId)
-          .orElseThrow(() -> new ResourceNotFoundException("Movie not found for this id :: " + movieId));
-        return ResponseEntity.ok().body(movie);
-    }
-    
-    @PostMapping("/movies")
-    public Movie createMovie(@Valid @RequestBody Movie movie) {
-        return moviesRepository.save(movie);
-    }
-
-
-
-
-}
-*/
